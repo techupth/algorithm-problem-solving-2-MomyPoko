d435e47295d477fca3afc675444deba8d8e2a0e8
Chavanvit <chavanvit.n@gmail.com>
2024-07-17 13:37:56 +0700

    feat: add algorithm exerxise 2

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,25 @@
 function findStudentIndex(students, searchStudent) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
+  // เริ่มเขียนโค้ดตรงนี้จ้า
+  for (let i = 0; i < students.length; i++) {
+    if (students[i] === searchStudent) {
+      return i;
+    }
+  }
+  return -1;
 }
 
+students = [
+  "Andrew",
+  "Bobby",
+  "Diana",
+  "George",
+  "Hannah",
+  "Isaac",
+  "Jasmine",
+  "John",
+];
+searchStudent = "John";
+console.log(findStudentIndex(students, searchStudent));
+
 // ตอบคำถามตรงนี้จ้า
+// O(n) เพราะว่าการค้นหาเรียงตาม loop 1 loop
